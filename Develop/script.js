e7ccdb4544eeb1d8e3304a56fab1701be27818b1
ltuckr <l.tucker.rdh@gmail.com>
2023-05-14 14:28:30 -0400

    added init functions for header time and block color

--- Develop/script.js
+++ Develop/script.js
@@ -16,8 +16,15 @@ var calEntryEventTime;
 var calEntryEventText;
 var timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
 
+//sitewide date and events
+$(function init() {
 
-$(function () {
-   init
+  headerDateTime(); 
+  renderEvent();
+  setBlockColor();
+
   });
+
+
+
   
