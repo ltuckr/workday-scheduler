8103ea1bdf3a89d1a72046f0aca5ba6fd0168597
ltuckr <l.tucker.rdh@gmail.com>
2023-05-18 11:11:34 -0400

    figured out why 8 AM time block wasn't working

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ var timerInterval;
 // Local storage
 var calEntryEventTime;
 var calEntryEventTxt;
-var timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+var timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 // Sitewide date and events
 $(function init() {
